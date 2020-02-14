8d7342c9fd7c19c661442e2c7afc1c0b6689f66e
unknown <sylvain.maurier@gmail.com>
2020-02-14 01:00:39 +0100

    45 over

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 
 export default class Connexion extends Component {
+
+    state = {
+        pseudo: '',
+        goToChat: false
+    }
+
+    handleChange = event => {
+        const pseudo = event.target.value
+        this.setState({pseudo})
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ goToChat:true })
+    }
+
     render() {
+        if(this.state.goToChat){
+            return <Redirect push to={`/pseudo/${this.state.pseudo}`}></Redirect>
+        }
+
         return (
             <div className="connexionBox">
-                <form className="connexion">
-                    <input placeholder="Pseudo" type="text" required />
+                <form className="connexion" onSubmit={this.handleSubmit}>
+                    <input placeholder="Pseudo" type="text" value={this.state.pseudo} onChange={this.handleChange}  required />
                     <button type="submit">GO</button>
                 </form>
             </div>
---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const Message = () => {
+    return (
+        <p className="user-message">
+            Mon message
+        </p>
+    )
+}
+
+export default Message
