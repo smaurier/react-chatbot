c1cb31b6179a5ec8dfb7844045911fb6c8705eae
unknown <sylvain.maurier@gmail.com>
2020-02-14 19:28:22 +0100

    48 over

--- src/App.js
+++ src/App.js
@@ -4,18 +4,28 @@ import Formulaire from './components/Formulaire'
 import Message from './components/Message'
 
 class App extends Component {
+
+  state = {
+    messages: {},
+    pseudo: this.props.match.params.pseudo
+  }
+
+  addMessage = message => {
+    const messages = { ... this.state.messages}
+    messages[`message-${Date.now()}`] = message
+    this.setState({messages})
+  }
+
   render () {
+    const messages = Object.keys(this.state.messages).map(key =>(<Message key={key} message={this.state.messages[key].message} pseudo={this.state.messages[key].pseudo} />))
     return (
       <div className='box'>
           <div>
             <div className="messages">
-              <Message />
-              <Message />
-              <Message />
-              <Message />
+              <div className="message"> {messages} </div>
             </div>
           </div>
-          <Formulaire />
+          <Formulaire length={140} pseudo={this.state.pseudo}  addMessage={this.addMessage} />
       </div>
     )
   }
--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
 
 export default class Formulaire extends Component {
+
+    state = {
+        message: '',
+        length: this.props.length
+    }
+
+    createMessage = () => {
+        const {addMessage, pseudo, length} = this.props
+        const message = {pseudo, message: this.state.message}
+        addMessage(message)
+        //reset message
+        this.setState({message:'', length})
+      }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.createMessage()
+    }
+
+    handleChange = event => {
+        const message = event.target.value
+        const length = this.props.length - message.length
+        this.setState({message, length});
+    }
+
+    handleKeyUp = event => {
+        if (event.key === 'Enter') {
+            this.createMessage()
+        }
+    }
+
     render() {
         return (
-            <form className="form"> 
-                <textarea required maxLength="140" />
-                <div className="info">140</div>
+            <form className="form" onSubmit={this.handleSubmit}>
+                <textarea value={this.state.message}  onChange={this.handleChange} onKeyUp={this.handleKeyUp} required maxLength={this.props.length} />
+                <div className="info">{this.state.length}</div>
                 <button type="submit">Envoyer !</button>
             </form>
         )
